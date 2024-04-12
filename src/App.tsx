2147c26ab2ee3decccd6163197d75b3c00431cfd
Kristjan Lääne <xonproject@gmail.com>
2024-04-12 17:43:49 +0300

    Exercise: Creating a Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import CourseGoal from './components/CourseGoals';
+import CourseGoal from './components/CourseGoals.tsx';
+import Header from './components/Header.tsx';
+import goals from './assets/goals.jpg';
 
 export default function App() {
   return (
     <main>
+      <Header image={{ src: goals, alt: 'Goals' }}>
+        <h1>Your Course Goals</h1>
+      </Header>
       <CourseGoal title='Learn React + TS'>
         <p>Learn it from the ground up</p>
       </CourseGoal>
