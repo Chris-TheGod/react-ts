0acb576c33fadc0d5403d5d26dddd5a29edb59d3
Kristjan Lääne <xonproject@gmail.com>
2024-04-18 13:57:11 +0300

    Problem: Flexible Components With Required Prop Combinations

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -2,10 +2,11 @@ import { ReactNode } from 'react';
 
 type InfoBoxProps = {
   mode: 'hint' | 'warning';
+  severity: 'low' | 'medium' | 'high';
   children: ReactNode;
 };
 
-export const InfoBox = ({ mode, children }: InfoBoxProps) => {
+export const InfoBox = ({ mode, severity, children }: InfoBoxProps) => {
   if (mode === 'hint') {
     return (
       <aside className='infobox infobox-hint'>
@@ -15,7 +16,7 @@ export const InfoBox = ({ mode, children }: InfoBoxProps) => {
   }
 
   return (
-    <aside className='infobox infobox-warning warning--medium'>
+    <aside className={`infobox infobox-warning warning--${severity}`}>
       <h2>Warning</h2>
       <p>{children}</p>
     </aside>
